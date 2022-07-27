41fa8d91371331df34f5a86d219270e440632e4c
EfrenOR <efren.or@outlook.com>
2022-07-26 23:52:32 -0500

    Menu para móvil funcional y se agrego página para busquedas

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -4,24 +4,35 @@ export default function buttons(){
 
     const $videoHeader = document.getElementById('video_background'),
           $btnSoundOn = document.getElementById('sound-on'),
-          $btnSoundOff = document.getElementById('sound-off')
-                
-          document.addEventListener('click', e=>{
+          $btnSoundOff = document.getElementById('sound-off'),
+          $btnMenu = document.getElementById('menu-hamburger'),
+          $menuMobile = document.querySelector('.menu-mobile'),
+          $menuMobileClose = document.getElementById('btn-close-menu')
 
+            document.addEventListener('click', e=>{
+                switch(e.target){
 
-            switch(e.target){
-                case $btnSoundOff:
-                    $videoHeader.muted = false;
-                    e.target.style = 'display: none';
-                    $btnSoundOn.style = 'display: inline-block';
-                break;
+                    case $btnSoundOff:
+                        $videoHeader.muted = false;
+                        e.target.style = 'display: none';
+                        $btnSoundOn.style = 'display: inline-block';
+                    break;
 
-                case $btnSoundOn:
-                    $videoHeader.muted = true;
-                    e.target.style = 'display: none';
-                    $btnSoundOff.style = 'display: inline-block';
-                break;
-            }
+                    case $btnSoundOn:
+                        $videoHeader.muted = true;
+                        e.target.style = 'display: none';
+                        $btnSoundOff.style = 'display: inline-block';
+                    break;
 
-          })
+                    case $btnMenu:
+                        $btnMenu.classList.add('is-active')
+                        $menuMobile.classList.add('is-active');
+                    break;
+
+                    case $menuMobileClose:
+                        $btnMenu.classList.remove('is-active')
+                        $menuMobile.classList.remove('is-active');
+                    break;
+                }
+            })
 }
